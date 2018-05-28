3be3bd6999853b85204d9dc4ca65302412cd3c5b
Gabriel Camelo <holysmoke003@gmail.com>
2018-05-28 19:42:00 -0300

    Update saveAsToPath.js

--- toonBoomHarmony/saveAsToPath.js
+++ toonBoomHarmony/saveAsToPath.js
@@ -1,23 +1,48 @@
+/*
+
+---[ Toon Boom Harmony Premium 14.0.0 ]---
+
+[PTBR] Esse script pede confirmação do usuária para salvar a cena e copiá-la em
+um caminho pré-determinado.
+
+.gabriel camelo @ birdo, mai/2018
+
+*/
+
 function saveAsToPath(){
 
-//FIXME Bugs acontecem se as pastas que tentam ser criadas já existem (?) !
-//FIXME checar se a pasta da cena (!!!) já existe. se sim, perguntar se que exluí-la pra gravar uma nova
+//DONE Bugs acontecem se as pastas que tentam ser criadas já existem (?) !
+//DONE checar se a pasta da cena (!!!) já existe. se sim, perguntar se que exluí-la pra gravar uma nova
+//DONE Diálogo explicativo!
+//FIXME possibilidade de mudar o caminho
 //FIXME ignorar pasta 'frames'
 //FIXME para arquivos *xstage, copiar apenas versão atual!
+//FIXME ignorar arquivos com ~
 
 /*  - - { M A I N   F U N C T I O N } - -  */
 
-var dirHandler = new Dir;
+if(!initDialog()){
+  return;
+}
+
+scene.saveAll();
+
+var rootDir = new Dir;
+rootDir.path = "C:/Users/User/Desktop/cenaTeste/savePath/";
 var scenePath = scene.currentProjectPath();
-var savePath = "C:/Users/User/Desktop/cenaTeste/savePath/" + scene.currentScene();
+var savePath = new Dir;
+savePath.path = rootDir.path + scene.currentScene();
 
-dirHandler.mkdir("C:/Users/User/Desktop/cenaTeste/savePath/");
-dirHandler.mkdir(savePath);
+if(!rootDir.exists){
+  rootDir.mkdir();
+}
 
-print("=========================");
+if(savePath.exists){
+  savePath.rmdirs();
+}
 
+rootDir.mkdir(savePath.path);
 loopAndCopy(scenePath);
-
 MessageBox.information("Foi!");
 
 /*  - - { H E L P E R   F U N C T I O N S } - -  */
@@ -33,11 +58,10 @@ function loopAndCopy(path){
   l.shift();
   l.shift();
   for(item in l){
-    print(fileOrFolder(dir.path + "/" + l[item]));
     if(fileOrFolder(dir.path + "/" + l[item]) == "FOLDER"){
       var tempDir = new Dir;
       var dif = dir.path.replace(scenePath,"");
-      tempDir.path = savePath + dif + "/" + l[item];
+      tempDir.path = savePath.path + dif + "/" + l[item];
       if(!tempDir.exists){
         tempDir.mkdir(tempDir.path);
       }
@@ -45,7 +69,7 @@ function loopAndCopy(path){
    }else if(fileOrFolder(dir.path + "/" + l[item]) == "FILE"){
       var originalFilePath = dir.path + "/" + l[item];
       var dif = dir.path.replace(scenePath,"");
-      var newFilePath = savePath + dif + "/" + l[item];
+      var newFilePath = savePath.path + dif + "/" + l[item];
 
       copyFileBytes(originalFilePath,newFilePath);
    }
@@ -73,6 +97,39 @@ function fileOrFolder(fileToCheck){
     return "FOLDER";
   }else{
     return "FILE";
+  }
+}
+
+function initDialog(){
+  var d = new Dialog();
+  d.title = "Save as to path...";
+  d.width = 350;
+
+  var s1 = "A cena atual será salva e copiada para o seguinte caminho.";
+  var l1 = new Label();
+  l1.text = s1;
+  d.add(l1);
+
+  var sPath = "C:/Users/User/Desktop/cenaTeste/savePath/";
+  var inputL = new LineEdit;
+  inputL.text = sPath;
+  inputL.label = "Save as to path:";
+  d.add( inputL );
+
+  var s2 = "ATENÇÃO!";
+  var l2 = new Label();
+  l2.text = s2;
+  d.add(l2);
+
+  var s3 = "Se já existir uma cena de mesmo nome nesse caminho, essa cena será excluída e\numa nova cena será salva em seu lugar.";
+  var l3 = new Label();
+  l3.text = s3;
+  d.add(l3);
+
+  if(d.exec()){
+	   return true;
+  }else{
+    return false;
   }
 }
 
