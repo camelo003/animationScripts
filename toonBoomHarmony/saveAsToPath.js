91386a330b1a0af25e3bb91b9b9c1df3f6108abc
Gabriel Camelo <holysmoke003@gmail.com>
2018-09-26 12:58:37 -0300

    Versão antiga para 'temp' + separa episódio

--- toonBoomHarmony/saveAsToPath.js
+++ toonBoomHarmony/saveAsToPath.js
@@ -5,7 +5,10 @@
 [PTBR] Esse script pede confirmação do usuário para salvar a cena e copiá-la em
 um caminho pré-determinado.
 
-.gabriel camelo @ birdo, mai/2018
+.gabriel camelo @ birdo, mai/2018,
+
+(o)>
+(v)_
 
 */
 
@@ -19,12 +22,22 @@ function saveAsToPath(){
 //DONE ignorar pasta 'frames'
 //DONE para arquivos *xstage, copiar apenas versão atual!
 //DONE ignorar arquivos com ~
+//DONE checar se o nome da cena está no padrao
+//DONE ao salvar uma cena, checar se ela já existe no destino
+//     se já existir, mandar a atual versão para a pasta 'temp/ep' + timestamp
+//DONE definir path com o ep correspondente
+
+//TODO (pro futuro) as funcoes aprovToTemp() e loopAndCopy() devem ser refatoradas em uma so!
 
 /*  - - { M A I N   F U N C T I O N } - -  */
 
 var rootDir = new Dir;
 //ALTERAR DESTINO PADRAO  NA LINHA ABAIXO!
-rootDir.path = "C:/Users/User/Desktop/cenaTeste/savePath/";
+rootDir.path = "C:/Users/gabriel.camelo/Desktop/testHarmonyScene/aprov";
+
+if(!checaPadraoNome()){
+  return;
+}
 
 if(!initDialog()){
   return;
@@ -34,17 +47,20 @@ scene.saveAll();
 
 var scenePath = scene.currentProjectPath();
 var savePath = new Dir;
-savePath.path = rootDir.path + "/" + scene.currentScene();
+savePath.path = rootDir.path + "/" + String(scene.currentScene().split("_")[1]) + "/" + String(scene.currentScene());
 
 if(!rootDir.exists){
   rootDir.mkdir();
 }
 
 if(savePath.exists){
+  aprovToTemp(savePath.path,"C:/Users/gabriel.camelo/Desktop/testHarmonyScene/temp","C:/Users/gabriel.camelo/Desktop/testHarmonyScene/temp");
   savePath.rmdirs();
+}else{
+  savePath.mkdir(rootDir.path + "/" + String(scene.currentScene().split("_")[1]));
 }
 
-rootDir.mkdir(savePath.path);
+savePath.mkdir(savePath.path);
 loopAndCopy(scenePath);
 MessageBox.information("A cena foi salva no\ncaminho especificado.");
 
@@ -52,6 +68,41 @@ MessageBox.information("A cena foi salva no\ncaminho especificado.");
 
 function print(content){
   MessageLog.trace(content);
+}
+
+function dataHora(){
+	var d = new Date();
+  var mes = String((d.getMonth()+1)); if(mes.length < 2){ mes = "0" + mes;}
+  var dia = String(d.getDate()); if(dia.length < 2){ dia = "0" + dia;}
+	var hora = String(d.getHours()); if(hora.length < 2){ hora = "0" + hora;}
+	var minuto = String(d.getMinutes()); if(minuto.length < 2){ minuto = "0" + minuto;}
+	var a = d.getFullYear() + mes + dia + "_" + hora + "h" + minuto;
+	return a;
+}
+
+function aprovToTemp(source,destiny,destinyRoot){
+  //FIXME -> essa coisa de destinyRoot está bem estranha. Melhorar!
+  var temp = new Dir;
+  if(destiny==destinyRoot){
+	temp.path = destiny + "/" + scene.currentScene() + "_" + dataHora();
+  }else{
+	temp.path = destiny;
+  }
+  temp.mkdir();
+
+  var aprov = new Dir;
+  aprov.path = source; //FIXME filtrar por cena
+  var sourceContent = aprov.entryList("*",-1,4);
+  sourceContent.shift();
+  sourceContent.shift();
+
+  for(item in sourceContent){
+    if(fileOrFolder(source + "/" + sourceContent[item]) == "FOLDER"){
+      aprovToTemp(source + "/" + sourceContent[item], temp.path + "/" +  sourceContent[item],destinyRoot);
+    }else if(fileOrFolder(source + "/" + sourceContent[item]) == "FILE"){
+	copyFile(source + "/" + sourceContent[item],temp.path+ "/" + sourceContent[item]);
+    }
+  }
 }
 
 function loopAndCopy(path){
@@ -97,11 +148,22 @@ function copyFile(copyPath,pastePath){
 }
 
 function fileOrFolder(fileToCheck){
-  var tempFile = new File(fileToCheck);
-  if(tempFile.size==0){
+  var tempFile = new QFileInfo(fileToCheck);
+  if(!tempFile.isFile()){
     return "FOLDER";
   }else{
     return "FILE";
+  }
+}
+
+function checaPadraoNome(){
+  var scArr = scene.currentScene().split("_");
+
+  if(scArr.length != 3){
+    MessageBox.information("O nome desta cena não está no padrão.\nImpossível mandar para aprovação!");
+    return false;
+  }else{
+    return true;
   }
 }
 
@@ -117,7 +179,7 @@ function initDialog(){
 
   var inputL = new LineEdit;
   inputL.text = rootDir.path;
-  inputL.label = "Save as to path:";
+  inputL.label = "Mandar para aprovação:";
   d.add( inputL );
 
   var s2 = "ATENÇÃO!";
@@ -125,7 +187,7 @@ function initDialog(){
   l2.text = s2;
   d.add(l2);
 
-  var s3 = "Se já existir uma cena de mesmo nome nesse caminho, essa cena será excluída e\numa nova cena será salva em seu lugar.";
+  var s3 = "Se já existir uma cena de mesmo nome nesse caminho, a versão anterior vai para um temporário\ne a nova cena será salva em seu lugar.";
   var l3 = new Label();
   l3.text = s3;
   d.add(l3);
